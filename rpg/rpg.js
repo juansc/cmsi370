5b2b048afdaa2bc185620dba547d071905876091
Juan S Carrillo <juanscarrillo23@gmail.com>
2014-10-27 21:25:35 -0700

    Added ability to choose a character.

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -8,8 +8,17 @@ $(function() {
                 tr.find(".class").text(character.classType);
                 tr.find(".gender").text(character.gender);
                 tr.find(".level").text(character.level);
+                tr.data('id',character.id)
                 return tr;
             }));
         }
     );
+});
+
+
+$(function(){
+    $('table').on('click', 'tr' , function (event) {
+        $('.active-character-row').removeClass('active-character-row'); 
+        $(this).addClass('active-character-row'); 
+    });
 });
