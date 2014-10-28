5dd17357891e93e6a97e5dc0e343322748e20c8f
Juan S Carrillo <juanscarrillo23@gmail.com>
2014-10-28 01:55:32 -0700

    Player card now reacts to menu.

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -8,7 +8,14 @@ $(function() {
                 tr.find(".class").text(character.classType);
                 tr.find(".gender").text(character.gender);
                 tr.find(".level").text(character.level);
-                tr.data('id',character.id)
+                tr.data('info',{
+                    'id':character.id,
+                    'name': character.name,
+                    'money': character.money,
+                    'gender': character.gender,
+                    'level': character.level,
+                    'class': character.classType
+                });
                 return tr;
             }));
         }
@@ -18,7 +25,15 @@ $(function() {
 
 $(function(){
     $('table').on('click', 'tr' , function (event) {
+        $('.character-card').addClass('rtable');
+        $('.character-card').removeClass('inactive-rtable');
         $('.active-character-row').removeClass('active-character-row'); 
-        $(this).addClass('active-character-row'); 
+        $(this).addClass('active-character-row');
+        var charInfo = $(this).data('info');
+        $(".character-card").find('.name').text(charInfo['name']);
+        $(".character-card").find('.level').text(charInfo['level']);        
+        $(".character-card").find('.gender').text(charInfo['gender']);        
+        $(".character-card").find('.class').text(charInfo['class']);        
+        $(".character-card").find('.money').text(charInfo['money']);                
     });
 });
