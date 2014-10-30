c230db2d11f7ed73fed4f906bd235658c7bf8893
Juan S Carrillo <juanscarrillo23@gmail.com>
2014-10-30 00:59:57 -0700

    Made edit dialog box that filters out bad input.

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -7,7 +7,6 @@ $(function() {
         function (characters) {
             $("tbody").append(characters.map(function (character) {
                 characterCount++;
-                console.log('characterCount is ' + characterCount);                
                 var tr = $(".character-template").clone();
                 tr.find(".name").text(character.name);
                 tr.find(".class").text(character.classType);
@@ -28,6 +27,7 @@ $(function() {
 
     $('table').on('click', 'tr' , function (event) {
         $('.trash-btn').removeClass('disabled');
+        $('.edit-btn').removeClass('disabled');
         $('.character-card').addClass('rtable');
         $('.character-card').removeClass('inactive-rtable');
         $('.active-character-row').removeClass('active-character-row'); 
@@ -37,17 +37,17 @@ $(function() {
 
     $('#delete-char-btn').click( function(){
         characterCount = characterCount > 1 ? characterCount - 1 : 0;
-        console.log('characterCount is ' + characterCount);
         var rowToRemove = $(".active-character-row");
         deleteChar(rowToRemove.data('info')['id']);
         var nextActiveRow = rowToRemove.closest('tr').next();
         nextActiveRow.addClass('active-character-row');
         rowToRemove.removeClass('active-character-row').remove();
         var info = nextActiveRow.data('info');
-        if( characterCount === 0 || $('.active-character-row').length === 0 ){
+        if( $('#char-table >tbody >tr').length === 0 || $('.active-character-row').length === 0 ){
             $('.character-card').remove('rtable');            
             $('.character-card').addClass('inactive-rtable');            
             $('.trash-btn').addClass('disabled');
+            $('.edit-btn').addClass('disabled');            
             info = {
                 'name':'',
                 'level':'',
@@ -76,4 +76,15 @@ $(function() {
             }
         });
     };
+
+    $(".edit-text-input-form").blur( function(){
+        $("#save-char-btn").removeClass('disabled');        
+        $(".edit-text-input-form").each( function(){
+            if($(this).val().localeCompare("") === 0 ){ $("#save-char-btn").addClass("disabled");}
+        });
+    });
+
+    $(".edit-number-input-form").blur(function(){
+        $(this).val( +$(this).val() || 0);
+    });
 });
