efdc03678c05e4f568ef4a9a49bfdc2b65d14d6d
Juan S Carrillo <juanscarrillo23@gmail.com>
2014-12-04 00:02:49 -0800

    Put it in a jQuery ready function.

--- widget-from-scratch/moving-tooltip.js
+++ widget-from-scratch/moving-tooltip.js
@@ -1,46 +1,54 @@
-$.fn.addMovingToolTip = function (str, height, width) {
-    var tooltipTimeout;
-    var length = str.length;
-    var tooltipHeight = height || Math.floor(length / 80 + 1);
-    var tooltipWidth = width || 4 / 3 * height;
-    var tooltip, tooltipX, tooltipY;
-    this.mousemove(function (event) {
-        tooltipX = event.pageX;
-        tooltipY = $(tooltip).parent().offset().top + 30; 
-        $(tooltip).offset({
-            top: tooltipY,
-            left: tooltipX 
-        });
-        //event.stopPropagation();
-    });
-    this.hover(function () {
-        tooltip = $("<div><div>")
-            .addClass("tooltip")
-            .appendTo("body")
-            .width(tooltipWidth)
-            .height(tooltipHeight)
-            .hide()
-            .html(str)
-            .offset({
+$(document).ready( function() {
+    $.fn.addMovingToolTip = function (str, height, width) {
+        var tooltipTimeout;
+        var length = str.length;
+        var tooltipHeight = height || Math.floor(length / 80 + 1);
+        var tooltipWidth = width || 4 / 3 * height;
+        var tooltip, tooltipX, tooltipY;
+        var elem = $(this);
+        this.mousemove(function (event) {
+            tooltipX = event.pageX;
+            tooltipY = $(tooltip).parent().offset().top + 30; 
+            console.log(" has x: " + tooltipX + " and y: " + tooltipY);
+            tooltip.offset({
                 top: tooltipY,
-                left: tooltipX
+                left: tooltipX 
             });
-        tooltipTimeout = setTimeout(showToolTip, 500);
-    },function () {
-        tooltip.fadeOut().remove();
-        tooltip = null;
-    });
+            //event.stopPropagation();
+        });
+        this.hover(function () {
+            console.log("Appearing has x: " + tooltipX + " and y: " + tooltipY)
+            tooltip = $("<div><div>")
+                .addClass("tooltip")
+                .appendTo(elem)
+                .width(tooltipWidth)
+                .height(tooltipHeight)
+                .hide()
+                .html(str)
+                .offset({
+                    top: tooltipY,
+                    left: tooltipX 
+                });
+                tooltipTimeout = setTimeout(showToolTip, 500);
+        },function () {
+            tooltip.remove();
+        });
 
-    var showToolTip = function () {
-        tooltip.show();
+        var showToolTip = function () {
+            tooltip.show();
+            console.log(tooltip.offset().top);
+            console.log(tooltip.offset().left);
+        };
+        var hideToolTip = function () {
+            console.log(tooltip.offset().top);
+            console.log(tooltip.offset().left);
+            tooltip.remove();
+        };
+
     };
-    var hideToolTip = function () {
-        console.log(tooltip);
-        tooltip.fadeOut().remove();
-    };
-
-};
 
 
 
-$("#test").addMovingToolTip("Just some string", 50, 100);
+    $("#test").addMovingToolTip("Just some string", 50, 100);
+
+});
