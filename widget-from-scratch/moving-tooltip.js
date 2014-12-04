46c4d25e2e983f11a04c71276f8d06ef3125a5ca
Juan S Carrillo <juanscarrillo23@gmail.com>
2014-12-03 23:39:21 -0800

    Got tooltip to appear.

--- widget-from-scratch/moving-tooltip.js
+++ widget-from-scratch/moving-tooltip.js
@@ -1,23 +1,46 @@
-$.fn.addMovingToolTip(str, height, width){
-    var mouseX, mouseY;
-    this.mousemove( function(event) {
-        $(".tooltip").offset({
-            top: event.pageY + 20,
-            left: event.pageX
-        });        
-    });
+$.fn.addMovingToolTip = function (str, height, width) {
     var tooltipTimeout;
     var length = str.length;
-    var tooltipHeight = height || Math.floor(length/80 + 1);
-    var tooltipWidth =  width || 4/3*height;
-    var tooltip = $("<div><div>")
-        .addClass("tooltip")
-        .css("visibility", "hidden")
-        .width(tooltipWidth)
-        .height(tooltipHeight)
-        .html(str);    
-    this.hover( function() {
-        tooltipTimeout = setTimeout(tooltip.css("visibility", "visible"), 500);
-    }, 
-    tooltip.css("visibility", "hidden"));
-}
+    var tooltipHeight = height || Math.floor(length / 80 + 1);
+    var tooltipWidth = width || 4 / 3 * height;
+    var tooltip, tooltipX, tooltipY;
+    this.mousemove(function (event) {
+        tooltipX = event.pageX;
+        tooltipY = $(tooltip).parent().offset().top + 30; 
+        $(tooltip).offset({
+            top: tooltipY,
+            left: tooltipX 
+        });
+        //event.stopPropagation();
+    });
+    this.hover(function () {
+        tooltip = $("<div><div>")
+            .addClass("tooltip")
+            .appendTo("body")
+            .width(tooltipWidth)
+            .height(tooltipHeight)
+            .hide()
+            .html(str)
+            .offset({
+                top: tooltipY,
+                left: tooltipX
+            });
+        tooltipTimeout = setTimeout(showToolTip, 500);
+    },function () {
+        tooltip.fadeOut().remove();
+        tooltip = null;
+    });
+
+    var showToolTip = function () {
+        tooltip.show();
+    };
+    var hideToolTip = function () {
+        console.log(tooltip);
+        tooltip.fadeOut().remove();
+    };
+
+};
+
+
+
+$("#test").addMovingToolTip("Just some string", 50, 100);
