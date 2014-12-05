84cb5fbfa58fc7ca50ac5890f065042e2dd580c0
Juan S Carrillo <juanscarrillo23@gmail.com>
2014-12-04 18:57:09 -0800

    Tooltips are now sticky by default.

--- widget-from-scratch/moving-tooltip.js
+++ widget-from-scratch/moving-tooltip.js
@@ -1,43 +1,53 @@
 $(document).ready(function () {
-    $.fn.addMovingToolTip = function (str, height, width) {
+    $.fn.addMovingToolTip = function (str) {
         var tooltipTimeout;
         var length = str.length;
-        //var tooltipHeight = height || Math.floor(length / 80 + 1)*10;
-        //var tooltipWidth = width || 4 / 3 * height;
         var tooltip, tooltipX, tooltipY;
         var elem = $(this);
         this.mousemove(function (event) {
             tooltipX = event.pageX;
-            tooltipY = $(tooltip).parent().offset().top + 30;
+            tooltipY = $(tooltip).parent().offset().top+20;
             tooltip.offset({
                 top: tooltipY,
                 left: tooltipX
             });
         });
-        this.hover(function () {
-            tooltip = $("<div><div>")
-                .addClass("tooltip")
-                .appendTo(elem)
-                //.width(tooltipWidth)
-                //.height(tooltipHeight)
-                .html(str)
-                .css("visibility", "hidden")
-                .offset({
-                top: tooltipY,
-                left: tooltipX
-            });
-            tooltipTimeout = setTimeout(showToolTip, 1000);
-        }, function () {
-            tooltip.remove();
+        this.mouseenter(function () {
+            if(!tooltip){
+                tooltip = $("<div><div>")
+                    .addClass("tooltip")
+                    .appendTo(elem)
+                    .html(str + "<br><font size=\"1\">Click to dismiss</font>")
+                    .css("visibility", "hidden")
+                    .offset({
+                    top: tooltipY,
+                    left: tooltipX
+                }).mousemove(function(event){
+                    event.stopPropagation();                
+                }).mouseenter(function(event){
+                    event.stopPropagation();
+                }).click(function(event){
+                    tooltip.remove();
+                    tooltip = null;                    
+                });
+                tooltipTimeout = setTimeout(showToolTip, 1000);
+            }
+        });
+        this.mouseleave(function (){
+            window.clearTimeout(tooltipTimeout);
+            tooltip = null;
         });
 
         var showToolTip = function () {
+            tooltipTimeout = null;
             $(tooltip).css("visibility", "visible");
 
         };
 
     };
 
-    $("#test").addMovingToolTip("");
+    $("#test").addMovingToolTip("Hello, there!!");
+    $("#test2").addMovingToolTip("I hate you...");
 
 });
+
